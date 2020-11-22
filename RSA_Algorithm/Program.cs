86e1c4405401347e6c05ed031bc8a1a81cbf6029
Xardann <48299208+Xardann@users.noreply.github.com>
2020-11-22 12:27:22 +0100

    first commit

---
+++ RSA_Algorithm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+
+namespace RSA_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            ClassLibrary.Class1.RSA();
+        }
+    }
+}
